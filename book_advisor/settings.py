aaae1aa82b8eb439d3f4bc6d2d01ef884924e3c9
MadalinNitu <madalin-constantin.nitu@my.fmi.unibuc.ro>
2019-03-24 16:52:11 +0000

    solve some bugs

--- book_advisor/settings.py
+++ book_advisor/settings.py
@@ -103,6 +103,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
--- rest_api/views.py
+++ rest_api/views.py
@@ -9,8 +9,24 @@ from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 from django.contrib.auth import logout, authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
-import uuid
+from django.views.decorators.csrf import csrf_exempt
 
+
+
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
+from rest_framework.response import Response
+
+
+import uuid
 import json
 
 from .models import *
@@ -18,25 +34,15 @@ from .models import *
 # Create your views here.
 
 
-class CustomAuthToken(ObtainAuthToken):
-
-	def post(self, request, *args, **kwargs):
-	    serializer = self.serializer_class(data=request.data,
-	                                       context={'request': request})
-	    serializer.is_valid(raise_exception=True)
-	    user = serializer.validated_data['user']
-	    token, created = Token.objects.get_or_create(user=user)
-	    return Response({'token': token.key, 'user_id': user.pk, 'email': user.email })
-
-
-
-
+@csrf_exempt
 @api_view(['POST'])
+@permission_classes((AllowAny,))
 def login_user(request):
 	msg = ''
 	if request.user.is_authenticated:
 		msg = 'Session already authenticated'
-		return JsonResponse({ 'has_error': 'true', 'msg': msg,})
+		return Response({ 'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 	else: # Not authenticated user
 		username = request.POST.get('username', None)
 		password = request.POST.get('password', None)
@@ -55,7 +61,8 @@ def login_user(request):
 			ok_param = False
 
 		if not ok_param:
-			return JsonResponse({ 'has_error': 'true', 'msg': msg,})
+			return Response({ 'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 
 		user = authenticate(username=username, password=password)
 		if user is not None: # Authenticate user
@@ -66,23 +73,29 @@ def login_user(request):
 					token = created
 				msg = 'User has been successfully authenticate'
 				return Response({ 'has_error': 'false', 'token': str(token), 
-					'msg': msg,})
+					'msg': msg,},
+                        status=HTTP_200_OK)
 			else: # Inactiv user
 				msg = 'User is set to inactiv'
-				return JsonResponse({'has_error': 'true', 'msg': msg,})
+				return Response({'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 		else: # Not authenticated after created
 			msg = 'Error login user'
-			return JsonResponse({'has_error': 'false', 'msg': msg,})
+			return Response({'has_error': 'false', 'msg': msg,},
+                        status=HTTP_404_NOT_FOUND)
 
 
 
 
+@csrf_exempt
 @api_view(['POST'])
+@permission_classes((AllowAny,))
 def register_user(request):
 	msg = ''
 	if request.user.is_authenticated:
 		msg = 'Session already authenticated'
-		return JsonResponse({ 'has_error': 'true', 'msg': msg,})
+		return Response({ 'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 	else: # Not authenticated user
 		username = request.POST.get('username', None)
 		password = request.POST.get('password', None)
@@ -101,14 +114,16 @@ def register_user(request):
 			ok_param = False
 
 		if not ok_param:
-			return JsonResponse({ 'has_error': 'true', 'msg': msg,})
+			return Response({ 'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 
 		# check if already exist an user with this username
 		user_already_exist = User.objects.filter(username=username)
 		if user_already_exist.first() is not None:
 			# username already exist ... 
 			msg = 'This user already exist'
-			return JsonResponse({ 'has_error': 'true', 'msg': msg,})
+			return Response({ 'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 		else:
 			user = User(username=username, first_name=first_name, last_name=last_name, email=email)
 			user.set_password(password)
@@ -122,87 +137,75 @@ def register_user(request):
 					token = created
 
 				msg = 'User has been created'
-				return JsonResponse({ 'has_error': 'false', 'token': str(token), 
-					'msg': msg,})
+				return Response({ 'has_error': 'false', 'token': str(token), 
+					'msg': msg,},
+                        status=HTTP_200_OK)
 			else: # Inactiv user
 				msg = 'User is set to inactiv'
-				return JsonResponse({'has_error': 'true', 'msg': msg,})
+				return Response({'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 		else: # Not authenticated after created
 			msg = 'Error creating user'
-			return JsonResponse({'has_error': 'false', 'msg': msg,})
+			return Response({'has_error': 'false', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 
 
+@csrf_exempt
 @api_view(['POST'])
 def logout_user(request):
 	msg = ''
 	if request.user.is_authenticated:
 		# Authenticated user
 		try:
+			request.user.auth_token.delete()
+		except (AttributeError, ObjectDoesNotExist):
+			msg = 'Token does not exist'
+			return Response({'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
+
+		try:
 			logout(request)
 			msg = 'Logout successfully'
 		except Exception as e:
 			msg = 'An error has occur in logout method: ' + str(e)
-			return JsonResponse({'has_error': 'true', 'msg': msg,})
+			return Response({'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 	
 		# User is logout now but in this session is still auth
-		return JsonResponse({'has_error': 'false', 'msg': msg,})
+		return Response({'has_error': 'false', 'msg': msg,},
+                        status=HTTP_200_OK)
 	else: # Not authenticated user
 		msg = 'Session are not authenticated'
-		return JsonResponse({'has_error': 'true', 'msg': msg,})
+		return Response({'has_error': 'true', 'msg': msg,},
+                        status=HTTP_400_BAD_REQUEST)
 
 
-
+@csrf_exempt
 @api_view(['GET', ])
 def get_recommanded_books(request):
 	msg = 'maintenance'
-	return JsonResponse({'has_error': 'false', 'msg': msg,})
+
+	return Response({'has_error': 'false', 'msg': msg,},
+                        status=HTTP_200_OK)
 
 
+@csrf_exempt
 @api_view(['GET', ])
 def get_books(request):
 	msg = 'maintenance'
-	return JsonResponse({'has_error': 'false', 'msg': msg,})
+
+	return Response({'has_error': 'false', 'msg': msg,},
+                        status=HTTP_200_OK)
 
 
+@csrf_exempt
 @api_view(['GET', ])
 def get_user(request):
 	msg = 'maintenance'
-	return JsonResponse({'has_error': 'false', 'msg': msg,})
-
-
-@api_view(['GET', 'POST', ])
-def index(request):
-	logout_status = None
-	if request.user.is_authenticated:
-		# Authenticated user
-		try:
-			logout(request)
-			logout_status = 'Logout successfully'
-		except Exception as e:
-			logout_status = 'An error has occur in logout method: ' + str(e)
 	
-		# User is logout now but in this session is still auth
-		return JsonResponse({'test':'1', 'logout_status': logout_status,})
-	else: # Not authenticated user
-		username = 'user1'
-		password = 'user1'
+	return Response({'has_error': 'false', 'msg': msg,},
+                        status=HTTP_200_OK)
 
-		already_exist = User.objects.filter(username=username)
-		if already_exist.first() is None: # If user not already exist
-			user = User(username=username, first_name='first_user', last_name='last_user', email='user@yahoo.com')
-			user.set_password(password)
-			user.save()
-			print("already_exist")
 
-		user = authenticate(username=username, password=password)
-		if user is not None: # Authenticate user
-			login(request, user)
-			if user.is_active: # User active
-				token, created = Token.objects.get_or_create(user=user)
-				if token is None: # Check if tocken already exist and return for login
-					token = created
-				return JsonResponse({'token': str(token), 'username': username, })
-			else: # Inactiv user
-				return JsonResponse({'test':'2'})
-		else: # Not authenticated
-			return JsonResponse({'test':'3'})
+
+
